c4020cc0b7e99c425527f203e77464f882ef6795
dodo <110053481+MdodoIng@users.noreply.github.com>
2025-04-02 22:40:11 +0530

    modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
@@ -7,10 +9,12 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   experimental: {
-    viewTransition : true
+    viewTransition: true,
   },
-  basePath:  "/me",
-  assetPrefix: "/me",
+  ...(isProd && {
+    basePath: "/me",
+    assetPrefix: "/me",
+  }),
 };
 
 export default nextConfig;
